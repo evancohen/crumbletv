e0fdf0ddd0153b3a490185eeb1e63859388c701b
Cody Stebbins <cody@trov.com>
2014-02-16 15:48:46 -0800

    Generate Main TypeScript instead of JavaScript Remove video.js since it does not support RTMP Live

---
+++ cookWebClient/test/spec/controllers/main.ts
@@ -0,0 +1,26 @@
+/// <reference path="../../../app/bower_components/dt-angular/angular-mocks.d.ts" />
+/// <reference path="../../../app/bower_components/dt-jasmine/jasmine.d.ts" />
+/// <reference path="../../../app/scripts/controllers/main.ts" />
+
+'use strict';
+
+describe('Controller: MainCtrl', () => {
+
+  // load the controller's module
+  beforeEach(module('cookWebClientApp'));
+
+  var MainCtrl: cookWebClientApp.MainCtrl,
+    scope: cookWebClientApp.IMainScope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(($controller: ng.IControllerService, $rootScope: ng.IRootScopeService) => {
+    scope = <any>$rootScope.$new();
+    MainCtrl = $controller('MainCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', () => {
+    expect(scope.awesomeThings.length).toBe(3);
+  });
+});
