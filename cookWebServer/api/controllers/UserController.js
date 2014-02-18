e5969f9fcd6ae580fc2e7c28052f626bc6a752c8
Cody Stebbins <cody@trov.com>
2014-02-18 09:23:17 -0800

    Add authenticated for session so authenticated policy works.

--- cookWebServer/api/controllers/UserController.js
+++ cookWebServer/api/controllers/UserController.js
@@ -36,6 +36,7 @@ module.exports = {
           if (match) {
             // password match
             req.session.user = user.id;
+            req.session.authenticated = true;
             res.json(user);
           } else {
             // invalid password
