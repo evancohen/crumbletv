e01c91461b23b21b86ad7fc16236ddf7f9d203ad
Cody Stebbins <cody@trov.com>
2014-02-22 18:25:04 -0800

    Lower case services for case sensitive linux

--- cookWebServer/api/controllers/BroadcastController.js
+++ cookWebServer/api/controllers/BroadcastController.js
@@ -15,7 +15,7 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
-var responseService = require('../Services/Response.js');
+var responseService = require('../services/Response.js');
 
 module.exports = {
     
