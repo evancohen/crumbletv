aa19094ca6d8ff4aee4c72b56462e229b33465e9
Cody Stebbins <cody@trov.com>
2014-03-02 20:58:25 -0800

    Add static copy to grunt file to optimze grunt build Add survey link to main page.

--- basil/Gruntfile.js
+++ basil/Gruntfile.js
@@ -270,7 +270,8 @@ module.exports = function (grunt) {
             'views/{,*/}*.html',
             'bower_components/**/*',
             'images/{,*/}*.{webp}',
-            'fonts/*'
+            'fonts/*',
+            'static/{,*/}*'
           ]
         }, {
           expand: true,
