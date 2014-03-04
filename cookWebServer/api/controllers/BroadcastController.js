3109328e3fc62885ded3bce321142c48c4387352
Cody Stebbins <cody@trov.com>
2014-03-03 21:55:41 -0800

    Using some JS Foo to bind the objects methods to singletons with the correct this reference.

--- cookWebServer/api/controllers/BroadcastController.js
+++ cookWebServer/api/controllers/BroadcastController.js
@@ -1,11 +1,15 @@
 var ResponseService = require("../services/ResponseService.js");
 var BroadcastController = require("./classes/BroadcastController.js");
 var User = require("../models/User.js");
+var lodash = require("lodash");
 
 var BroadcastExport = new BroadcastController(User, new ResponseService());
 
-module.exports = {
-    publish: function (request, response) {
-        return BroadcastExport.publish(request, response);
+var exportSingleton = {};
+lodash.forIn(BroadcastExport, function (method, key) {
+    if (lodash.isFunction(method)) {
+        exportSingleton[key] = lodash.bind(method, BroadcastExport);
     }
-};
+});
+
+module.exports = exportSingleton;
--- cookWebServer/api/controllers/BroadcastController.ts
+++ cookWebServer/api/controllers/BroadcastController.ts
@@ -1,18 +1,31 @@
 /// <reference path="../../References.d.ts"/>
 
+
+
 // Dependencies for BroadcastController
 var ResponseService = <any>require("../services/ResponseService.js");
 var BroadcastController = <any>require("./classes/BroadcastController.js");
 var User = <any>require("../models/User.js");
+var lodash = require("lodash");
+
+
 
 // Exports BroadcastController with the necessary dependencies.
 // Since User is not defined when sails first runs, it must be passed in a simple factory.
 var BroadcastExport = new BroadcastController(User, new ResponseService());
 
-// TODO: Figure out a better way to force sails to use the prototype
-module.exports = {
-    publish: (request, response) => {
-        return BroadcastExport.publish(request, response);
+// Some serious JS foo here.
+var exportSingleton = {};
+lodash.forIn(BroadcastExport, function(method, key) {
+    if (lodash.isFunction(method)) {
+        exportSingleton[key] = lodash.bind(method, BroadcastExport);
+
     }
-};
+});
+
+
+// TODO: Figure out a better way to force sails to use the prototype
+module.exports = exportSingleton;
+
+
 
