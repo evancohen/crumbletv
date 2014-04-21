b5a073904ac61d1207d736a84b94c05026eb7515
jacesherrill <jacesher@uw.edu>
2014-04-20 22:27:16 -0700

    initial commit and add Event controller to sails

---
+++ cookWebServer/api/models/Event.js
@@ -0,0 +1,39 @@
+/**
+ * Event
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	
+  	title: {
+  		type: 'string',
+  		unique: false,
+  		required: true
+  	},
+
+  	startDate: {
+  		type: 'datetime',
+  		required: true
+  	},
+  	
+  	endDate: {
+  		type: 'datetime',
+  		required: true
+  	},
+  	start: 'string',
+  	end: 'string',
+  	owner: {
+  		model: 'user'
+  	},
+  	show: {
+  		model: 'show'
+  	}
+    
+  }
+
+};
