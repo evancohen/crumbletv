b2c6767e96aa82db2bdfdd90e7af90b7000f919e
Evan Cohen <evanbtcohen@gmail.com>
2014-05-04 19:14:12 -0700

    Authenitication service and Gravatars

---
+++ basil/dist/scripts/controllers/35c0493b.login.js
@@ -0,0 +1,24 @@
+angular.module("basilApp").controller("loginController", [
+  "$scope", 
+  "Restangular",
+  "gravatarService",
+  "authService",
+  function ($scope, Restangular, gravatarService, authService) {
+    //Things will happen here
+    $scope.email = "";
+    $scope.password = "";
+    $scope.success = "FALSE";
+    $scope.avatar = "http://placehold.it/80x80";
+
+    console.log(authService);
+
+    $scope.login = function() {
+      authService.authenticate($scope.email, $scope.password).then(function(res){
+        console.log(res);
+        gravatarService.getGravatar(res.name).then(function(img){
+          console.log(img);
+          $scope.avatar = img;
+        });
+      });
+    };
+}]);
