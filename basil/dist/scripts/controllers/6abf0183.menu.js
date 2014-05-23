1f3963fe95b63e53985450e6ab89549e59e6e6d6
Evan Cohen <evanbtcohen@gmail.com>
2014-05-22 17:38:09 -0700

    player. hella sexy!

--- basil/dist/scripts/controllers/6abf0183.menu.js
+++ basil/dist/scripts/controllers/6abf0183.menu.js
@@ -1,29 +1,28 @@
 angular.module("basilApp").controller("menuController", ["$scope", "authService", "gravatarService", "notificationService", function ($scope, authService, gravatarService, notificationService) {
     $scope.name = false;
-    $scope.avatar = false;
+    $scope.avatar = undefined;
 
     authService.getName().then(function(name){
     	$scope.name = name;
     }, function(err){
-    	console.log("We are not logged in");
+    	//we are not authorised
     	$scope.name = false;
     });
 
     $scope.logout = function(){
     	authService.logout().then(function(res){
     		$scope.name = false;
-    		$scope.avatar = false;
+    		$scope.avatar = undefined;
     	});
     }
 
     //when name is updated in the authService have it reflect on the page
     $scope.$watch( function () { return authService.name; }, function (name) {
-    	console.log("name changed!");
     	if(!(typeof name === "undefined") &&!(name === null)){
 			$scope.name = name;
 			//if we don't yet have an avatar set we should
 			//console.log($scope.avatar, name);
-			if($scope.avatar == false && name != false){
+			if($scope.avatar == undefined && name != false){
 				//console.log("Getting avatar");
 				gravatarService.getGravatar(name).then(function(img){
 					$scope.avatar = img;
@@ -36,14 +35,6 @@ angular.module("basilApp").controller("menuController", ["$scope", "authService"
         console.log("notification changed!");
         if(!(typeof notification === "undefined") &&!(notification === null)){
             $scope.notification = notification;
-            //if we don't yet have an avatar set we should
-            //console.log($scope.avatar, notification);
-            if($scope.avatar == false && notification != false){
-                //console.log("Getting avatar");
-                gravatarService.getGravatar(name).then(function(img){
-                    $scope.avatar = img;
-                });
-            }
         }
     });
 
