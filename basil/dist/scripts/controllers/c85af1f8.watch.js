33ae8304b93d3e4256c12b2ea1c59f1b03912922
Evan Cohen <evanbtcohen@gmail.com>
2014-05-26 18:49:56 -0700

    FireFox bugfix. Amazing!

--- basil/dist/scripts/controllers/c85af1f8.watch.js
+++
@@ -1,8 +0,0 @@
-angular.module("basilApp").controller("watchController", [
-	"$scope", 
-	'$stateParams',
-	function ($scope, $stateParams) {
-		//todo check the name passed in
-		$scope.streamName = $stateParams.name;
-	}
-]);
